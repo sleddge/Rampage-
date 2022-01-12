b6132b5a902a0747ded5d526d74efba4947d49b3
Chocolates15 <Voldemore@protonmail.com>
2022-01-12 14:33:14 +0100

    test

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-from Game import *
 import pygame
-pygame.init()
+from game import Game
 
 if __name__ == '__main__':
     pygame.init()
-    game = Game()
-    game.run()
-
-"""
-
-screen = pygame.display.set_mode((800, 800))
-running = True
-
-image = pygame.image.load("ball.png").convert()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.blit(image, (0, 0))
-
-    pygame.display.flip()
-
-pygame.quit()
-
-"""
+    myGame = Game()
+    myGame.run()
