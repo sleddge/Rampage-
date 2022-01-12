2aa3478203a079773c6d1e8a132ababbfa4ace4d
sleddge <berry.pierre71@gmail.com>
2022-01-12 14:28:08 +0100

    texture

---
+++ 1620294984-you.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="1620294984-you" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="texture pour jeu/1620294984-you.png" trans="000000" width="384" height="256"/>
+</tileset>
